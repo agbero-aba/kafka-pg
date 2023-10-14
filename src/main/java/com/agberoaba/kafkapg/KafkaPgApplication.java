5a24e2ea73a529d989dc8fb4a832ed879a755fd6
Fredrick Ugonna <fredgofer@gmail.com>
2023-10-14 13:03:51 +0200

    create producer component

---
+++ src/main/java/com/agberoaba/kafkapg/KafkaPgApplication.java
@@ -0,0 +1,18 @@
+package com.agberoaba.kafkapg;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaPgApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaPgApplication.class, args);
+  }
+
+  CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+    return args -> kafkaTemplate.send("file", "url");
+  }
+}
---
+++ src/test/java/com/agberoaba/kafkapg/KafkaPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agberoaba.kafkapg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
